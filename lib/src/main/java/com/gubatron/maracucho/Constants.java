63a674249456e10c9363b2204c51a9949788fc92
gubatron <gubatron@gmail.com>
2021-10-31 20:57:35 -0600

    Added STANDARD_MARABU_PORT and BOOSTRAP_HOSTNAME constants

--- lib/src/main/java/com/gubatron/maracucho/Constants.java
+++ lib/src/main/java/com/gubatron/maracucho/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
     public static final String VERSION = "0.1.1"; // MAJOR, MINOR, MONOTONIC_INCREMENTAL_BUILD
     public static final String AGENT_NAME = "Maracucho Client";
     public static final String AGENT = AGENT_NAME + " " + VERSION.substring(0, VERSION.lastIndexOf("."));
+    public static final int STANDARD_MARABU_PORT = 18018;
+    public static final String BOOSTRAP_HOSTNAME = "keftes.di.uoa.gr";
 }
