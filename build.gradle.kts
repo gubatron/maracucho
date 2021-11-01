f7338f2ddcb4a9bb3703ef1995710afd2a64b1bb
gubatron <gubatron@gmail.com>
2021-10-31 20:52:01 -0600

    build.gradle fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,15 @@
 
 plugins {
     // Apply the java-library plugin for API and implementation separation.
+    id("org.jetbrains.kotlin.jvm") version "1.5.0"
+    application
     `java-library`
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
 
 repositories {
@@ -17,12 +25,20 @@ repositories {
 }
 
 dependencies {
-    // Use JUnit test framework.
+    implementation("com.google.code.gson:gson:2.8.8")
     testImplementation("junit:junit:4.13.2")
+}
 
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api("org.apache.commons:commons-math3:3.6.1")
+sourceSets {
+    main {
+        java {
+            setSrcDirs(listOf("lib/src/main/java"))
+        }
+    }
 
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation("com.google.guava:guava:30.1.1-jre")
+    test {
+        java {
+            setSrcDirs(listOf("lib/src/test/java"))
+        }
+    }
 }
